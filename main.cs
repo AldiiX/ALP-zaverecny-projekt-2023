5a5e8a59fd90081efae68de4c08ae8694d0a4176
AldiiX <aldiix@email.cz>
2023-12-13 21:27:04 +0100

    1.3.8.6

---
+++ main.cs
@@ -0,0 +1,33 @@
+﻿// ALP - závěrečný projekt
+global using Spectre.Console;
+
+
+
+AnsiConsole.Markup("[yellow1]ALP - Závěrečný projekty[/]");
+AnsiConsole.Markup("[grey35]\n... IT1A\n\n\n\n[/]");
+
+string selectedProject = AnsiConsole.Prompt(
+    new SelectionPrompt<string>()
+    .Title("Vyber projekt, který se spustí")
+    .PageSize(15)
+    .AddChoices("Rock, Paper, Scissors Game", "Launcher Login Saver", "Bankovní Kalkulačka", "Interaktivní Kalendář")
+);
+
+switch(selectedProject) {
+    case "Rock, Paper, Scissors Game": {
+        Console.Clear();
+        RockPaperScissors.Main();
+    } break;
+    case "Launcher Login Saver": {
+        Console.Clear();
+        LauncherLoginSaver.Main(); 
+    } break;
+    case "Bankovní Kalkulačka": {
+            Console.Clear();
+            BankCalculator.Main();
+    } break;
+    case "Interaktivní Kalendář": {
+            Console.Clear();
+            InteractiveCalendar.Main();
+    } break;
+}
